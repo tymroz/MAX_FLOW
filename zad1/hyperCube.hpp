3c8d872a77631a8ee446dfd54f11973fc04a92af
tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2025-01-08 12:18:19 +0100

    poprawny kod dla zad1

--- zad1/hyperCube.hpp
+++ zad1/hyperCube.hpp
@@ -19,8 +19,12 @@ int zeroCount(int x, int k) {
 class Hypercube {
 private:
     int k;
+    int augmentPathCount;
+    int n;
     std::vector<std::vector<int>> edges;
     std::vector<std::vector<int>> capacity;
+    std::vector<std::vector<int>> flow;
+    
 
     bool areConnected(int i, int j) {
         int diff = i ^ j;
@@ -39,14 +43,40 @@ private:
         return dis(gen);
     }
 
+    bool bfs(int source, int target, std::vector<int>& parent) {
+        std::priority_queue<std::pair<int, int>> q;
+        std::vector<bool> visited(n);
+        q.push({INF, source});
+        parent[source] = -2;
+        visited[source] = true;
+
+        while (!q.empty()) {
+            auto [flowLeft, u] = q.top();
+            q.pop();
+            for (int v : edges[u]) {
+                if (!visited[v] && capacity[u][v] > flow[u][v]) {
+                    parent[v] = u;
+                    visited[v] = true;
+                    int newFlow = std::min(flowLeft, capacity[u][v] - flow[u][v]);
+                    q.push({newFlow, v});
+                    if (v == target) {
+                        augmentPathCount++;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 public:
-    Hypercube(int k) : k(k) {
-        edges.resize(1 << k); // 2^k wierzchołków
-        capacity.assign(1 << k, std::vector<int>(1 << k, 0));
-        flow.assign(1 << k, std::vector<int>(1 << k, 0));
-        for (int i = 0; i < (1 << k); ++i) {
+    Hypercube(int k) : k(k), augmentPathCount(0), n(1 << k) {
+        edges.resize(n); // 2^k wierzchołków
+        capacity.assign(n, std::vector<int>(n, 0));
+        flow.assign(n, std::vector<int>(n, 0));
+        for (int i = 0; i < (n); ++i) {
             int hamming_i = hammingWeight(i);
-            for (int j = i + 1; j < (1 << k); ++j) {
+            for (int j = i + 1; j < (n); ++j) {
                 int hamming_j = hammingWeight(j);
                 if (areConnected(i, j) && hamming_i < hamming_j) {
                     int weight = randomWeight(i, j);
@@ -58,7 +88,7 @@ public:
     }
 
     void printEdges() const {
-        for (int i = 0; i < (1 << k); ++i) {
+        for (int i = 0; i < (n); ++i) {
             std::cout << "Wierzcholek " << i << " (binarne: " << std::bitset<17>(i) << "):\n";
             for (int v : edges[i]) {
                 std::cout << "  --> Wierzcholek " << v << " (binarne: " << std::bitset<17>(v) << "), Waga: " << capacity[i][v] << "\n";
@@ -66,7 +96,40 @@ public:
         }
     }
 
-    int edmondsKarp(int source, int target) {
-        
+    void printFlow() const {
+        std::cout << "Przeplywy:\n";
+        for (int i = 0; i < (n); ++i) {
+            for (int v : edges[i]) {
+                std::cout << i << " -> " << v << ": " << flow[i][v] << "\n";
+            }
+        }
     }
+
+    int edmondsKarp(int source, int target) {
+        int maxFlow = 0;
+        std::vector<int> parent(n, -1);
+
+        while (bfs(source, target, parent)) {
+            int pathFlow = INF;
+            for (int v = target; v != source; v = parent[v]) {
+                int u = parent[v];
+                pathFlow = std::min(pathFlow, capacity[u][v] - flow[u][v]);
+            }
+
+            for (int v = target; v != source; v = parent[v]) {
+                int u = parent[v];
+                flow[u][v] += pathFlow;
+                flow[v][u] -= pathFlow;
+            }
+
+            maxFlow += pathFlow;
+        }
+
+        return maxFlow;
+    }
+
+    int getAugmentingPathsCount() const {
+        return augmentPathCount;
+    }
+
 };
--- zad1/test.cpp
+++ zad1/test.cpp
@@ -1,15 +1,28 @@
 #include "hyperCube.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
     int k;
-    std::cout << "Podaj liczbe bitow k: ";
-    std::cin >> k;
+    bool printFlow = false;
+
+    for (int i = 1; i < argc; i++) {
+        if (std::string(argv[i]) == "--size" && i + 1 < argc) {
+            k = atoi(argv[i+1]);
+        } else if (std::string(argv[i]) == "--printFlow") {
+            printFlow = true;
+        }
+    }
 
     Hypercube hypercube(k);
-    //std::cout << "1\n";
     hypercube.printEdges();
-    //int maxFlow = hypercube.edmondsKarp(0, (1 << k) - 1);
-    //std::cout << "Maksymalny przeplyw: " << maxFlow << std::endl; 
+
+    int maxFlow = hypercube.edmondsKarp(0, (1 << k) - 1);
+    int au = hypercube.getAugmentingPathsCount();
+    std::cout << "Maksymalny przeplyw: " << maxFlow << std::endl; 
+    std::cout << "Liczba sciezek powiekszajacych: " << au << std::endl; 
+
+    if (printFlow) {
+        hypercube.printFlow();
+    }
     
     return 0;
 }
