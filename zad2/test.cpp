e09cc6125497b3898e9ae7d2de07a9924bb0dbea
tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2025-01-10 18:18:30 +0100

    generowanie pliku .mod, z modelem programowania liniowego

--- zad2/test.cpp
+++ zad2/test.cpp
@@ -1,10 +1,13 @@
 #include "bipartieGraph.hpp"
 #include <chrono>
+#include <string>
 
 int main(int argc, char* argv[]) {
     int k;
     int i;
     bool printMatching = false;
+    std::string filename = "test.mod";
+    bool glpk = false;
 
     for (int j = 1; j < argc; j++) {
         if (std::string(argv[j]) == "--size" && j + 1 < argc) {
@@ -13,6 +16,9 @@ int main(int argc, char* argv[]) {
             i = atoi(argv[j+1]);
         } else if (std::string(argv[j]) == "--printMatching") {
             printMatching = true;
+        } else if (std::string(argv[j]) == "--glpk") {
+            filename = argv[j+1];
+            glpk = true;
         }
     }
 
@@ -26,10 +32,14 @@ int main(int argc, char* argv[]) {
     int time = duration.count();
 
     std::cout << "Maksymalne skojarzenie: " << maxMatching << std::endl;
-    std::cerr << "Czas: " << time << std::endl; 
+    std::cerr << "Czas: " << time << std::endl;
 
     if(printMatching){
         graph.displayMatching();
+    }
+
+    if(glpk){
+        graph.exportToGMPL(filename);
     }
     
     return 0;
